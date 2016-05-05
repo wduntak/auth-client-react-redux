cf3e9ca557e8353278951f56ea09bc177125a468
wduntak <wduntak@gmail.com>
2016-05-05 11:01:07 -0400

    Import axios for action creator signinUser

---
+++ src/actions/index.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const ROOT_URL = 'https://localhost:3030';
+
+export function signinUser({email, password}) {
+	return function(dispatch) {
+		// Submit email/password to the server
+		axios.post(`${ROOT_URL}/signin`, { email, password });
+		// If request is good...
+		// - Update state to indicate user is authenticated
+		// - Save the JWT token
+		// - Redirect to the route/feature
+
+		// If request is bad...
+		// - Show an error to the user
+		// 
+	}
+}
