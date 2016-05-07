2cee1c0a2e3bec11054cef70093967d00c0cb38a
wduntak <wduntak@gmail.com>
2016-05-07 12:07:42 -0400

    Setup action creator to fetch server response with JWT

--- src/actions/index.js
+++ src/actions/index.js
@@ -50,3 +50,13 @@ export function signoutUser() {
 	localStorage.removeItem('token');
 	return { type: UNAUTH_USER }
 }
+export function fetchMessage() {
+	return function(dispatch) {
+		axios.get(ROOT_URL, {
+			headers: { authorization: localStorage.getItem('token')}
+		})
+			.then(response => {
+				console.log(response);
+			})
+	}
+}
